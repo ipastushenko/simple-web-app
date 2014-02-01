b51698080cebe78129ffb3cf810678808445808b
Ivan Pastushenko <pastushenko.ivan@gmail.com>
2014-02-01 20:39:37 +0700

    Add: custom devise session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 SimpleWebApp::Application.routes.draw do
   root 'home#index'
 
+  devise_for :users, controllers: { sessions: 'sessions' }, skip: [:sessions]
+
   scope :api, format: false, defaults: { format: 'json' } do
     post 'message/create', to: 'message#create'
-  end
 
-  devise_for :users
+    devise_scope :user do
+      post 'signin', to: 'sessions#create'
+      delete 'signout', to: 'sessions#destroy'
+      get 'current_user', to: 'sessions#show_current_user'
+    end
+  end
 end
